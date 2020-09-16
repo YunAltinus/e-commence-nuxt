7a3c55774443799cf5350667018238a30d8d69f0
Yunus Altın <yunus_altin_1963@gmail.com>
2020-09-16 17:07:54 +0300

    created navbar and responsive

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,36 +1,130 @@
 <template>
-  <b-navbar toggleable="lg" type="dark" variant="danger">
-    <b-container>
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item href="#" >Link</b-nav-item>
-          <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#">Link</b-nav-item>
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template v-slot:button-content>
-              USER
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-container>
-  </b-navbar>
+  <header id="nav">
+    <nav>
+      <input type="checkbox" id="res-menu" />
+      <label for="res-menu">
+        <i id="sing-one" class="fa fa-bars"></i>
+        <i id="sing-two" class="fa fa-times"></i>
+      </label>
+      <ul>
+        <li>
+          <nuxt-link class="n-link" to="/" tag="a">Anasayfa</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link class="n-link" to="/" tag="a">PROFİL</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link class="n-link" to="/" tag="a">KATEGORİLER</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+    <div class="div-btn">
+      <button class="n-btn btn">
+        Çıkış
+        <nuxt-link to="/" tag="a"></nuxt-link>
+      </button>
+    </div>
+  </header>
 </template>
 
 <script>
-export default {}
+export default {
+  mounted() {
+    this.$nextTick(function() {
+      window.addEventListener("scroll", function() {
+        let navbar = document.querySelector("#nav");
+        let button = document.querySelector(".n-btn");
+        let nav_classes = navbar.classList;
+        if (document.documentElement.scrollTop >= 50) {
+          if (nav_classes.contains("shrink") === false) {
+            nav_classes.toggle("shrink");
+          }
+        } else {
+          if (nav_classes.contains("shrink") === true) {
+            nav_classes.toggle("shrink");
+          }
+        }
+      });
+    });
+  }
+};
 </script>
 
-<style>
-</style>
+<style lang="scss" scoped>
+header {
+  background-color: #122e50;
+  display: flex;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 31;
+  transition: 0.4s;
+  padding: 1rem 2.5rem;
+
+  #res-menu {
+    display: none;
+  }
+  label #sing-one,
+  label #sing-two {
+    float: left;
+    display: none;
+    color: #fff;
+    font-size: 25px;
+    line-height: 50px;
+    cursor: pointer;
+  }
+
+  nav {
+    ul {
+      padding-top: 0;
+      left: 0;
+      li {
+        display: inline-block;
+        padding: 0 0 5px 0;
+        margin-right: 40px;
+        .n-link {
+          text-transform: uppercase;
+          text-decoration: none;
+          color: white;
+          transition: all 0.4s;
+
+          &:hover,
+          &:active {
+            text-shadow: 1px 0 20px #1da1f2;
+            color: #64e0ff;
+            font-size: 16.2px;
+          }
+        }
+      }
+    }
+  }
+
+  .div-btn {
+    margin-top: 17px;
+    margin-left: auto;
+    max-height: 50px;
+
+    .n-btn {
+      text-transform: uppercase;
+      color: #96d7ff;
+      background-color: transparent;
+      border: 1px solid #96d7ff;
+      border-radius: 5px;
+      transition: all 0.2s;
+
+      &:hover {
+        color: #014aa3 !important;
+        border-color: #96d7ff !important;
+        background-color: #fff !important;
+        box-shadow: 1px 0 10px #96d7ff !important;
+        text-shadow: 1px 0 15px #96d7ff !important;
+        font-size: 16.1px;
+      }
+    }
+  }
+}
+
+.shrink {
+  padding: 0 25px;
+}
+</style>>
