8dfb8bc41681ba26962620a60afa846a85d3eaf6
Yunus Altın <yunus_altin_1963@gmail.com>
2020-09-16 20:57:04 +0300

    Create _id.vue

---
+++ pages/products/_id.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-container id="product-container">
+    <b-row>
+      <b-col lg="3" class="mb-3">
+        <SideBar class="mt-2" />
+      </b-col>
+      <b-col class="text-center">
+        <Product />
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import SideBar from "~/components/SideBar";
+import Product from "~/components/Product/Product";
+
+export default {
+  components: {
+    SideBar,
+    Product
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#product-container {
+  margin-top: 140px;
+}
+</style>
