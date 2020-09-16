67e542cc8da228228ee6a37144a0793a1a4961b9
Yunus Altın <yunus_altin_1963@gmail.com>
2020-09-16 17:07:05 +0300

    Created main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,37 @@
 <template>
-  <b-container>
-    <div class="container">
-      <div>
-        <logo />
-        <h1 class="title">e-commence-nuxt</h1>
-        <h2 class="subtitle">My doozie Nuxt.js project</h2>
-        <div class="links">
-          <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-        </div>
-      </div>
-    </div>
+  <b-container id="container">
+    <b-row>
+      <b-col md="4">
+        <SideBar class="mt-2" />
+      </b-col>
+      <b-col class="con-div">
+        <h2 class="text-center mt-4 mt-md-0">YENİ ÜRÜNLER</h2>
+        <ProductList />
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
 <script>
+import SideBar from "~/components/Home/SideBar";
+import ProductList from "~/components/Home/ProductList";
 
+export default {
+  components: {
+    SideBar,
+    ProductList
+  }
+};
 </script>
 
+<style lang="scss" scoped>
+#container {
+  margin-top: 140px;
+  .con-div {
+    text-align: center;
+    h2 {
+      color: #122E50;
+    }
+  }
+}
+</style>
